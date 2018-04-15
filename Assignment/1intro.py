b7169c6c4edb212d153ab4ebc12e99d9f5e0c130
KARAN GULERI <karanguleri.kd@gmail.com>
2018-04-16 02:05:34 +0530

    Add files via upload

--- Assignment/1intro.py
+++ Assignment/1intro.py
@@ -3,13 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-file =open('saved_data.csv',newline='')
-reader = csv.reader(file)
-header = next(reader)
+file =open('saved_data.csv','w')
+writer = csv.writer(file)
+
+writer.writerow(["File_Name","Location"])
+
+
 ls = ['DSC01713.JPG','DSC01798.JPG','DSC01836.JPG','DSC01886.JPG','DSC01916.JPG','DSC02013.JPG','DSC02209.JPG','DSC02252.JPG','DSC02407.JPG','DSC02426.JPG']
 for i in range(len(ls)):
     img = cv2.imread(ls[i])
-    print(ls[i]+':')
+    
+    File = ls[i]
     img_gry =cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     for angle in np.arange(0, 360,18.0):
 
@@ -21,10 +25,10 @@ for i in range(len(ls)):
         res = cv2.matchTemplate(img_gry,temp,cv2.TM_CCOEFF_NORMED)
 
         thresh = 0.85
+        arr=[]
         loc = np.where(res>=thresh)
         for pt in zip(*loc[::-1]):
-            print("YES")
-            print(pt[0]+w/2,pt[1]+h/2)
-    
+            arr.append([pt[0]+w/2,pt[1]+h/2])
+        writer.writerow([File,arr])
 
-#cv2.imshow('detected',img)
+
